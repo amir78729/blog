96177228aebc00d65a04bc8ed16130ef2b09c276
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-06-13 04:46:42 +0330

    fix: resolve build issues

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  output: "export",
+
   // basePath: "/blog",
   reactStrictMode: true,
 }
